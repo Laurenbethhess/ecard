999f9354c94a3765922f382ee8204668abdd51dd
Wasanta <kwan_u_day@hotmail.com>
2022-01-26 14:50:04 -0500

    first

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,9 @@ Card.destroy_all
 
 User.create(username: "Lauren", first_name: "Lauren", last_name: "Hess")
 Template.create(classname: "happy_birthday")
+Template.create(classname: "congrats")
+Template.create(classname: "get_well")
+Template.create(classname: "love")
 Card.create(user_id: 1, template_id: 1, salutation: "Dear", message: "Love you!", closing: "Love", receiver: "Caris")
 Card.create(user_id: 1, template_id: 4, salutation: "Dear", message: "Love you!", closing: "Love", receiver: "Caris")
 Card.create(user_id: 1, template_id: 5, salutation: "Dear", message: "Love you!", closing: "Love", receiver: "Caris")
